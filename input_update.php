5ed1ca9920c365446c4b39143adcbd53a5616770
윤희동 <jini1237@naver.com>
2022-07-04 02:08:21 +0900

    111111
    1111111

--- input_update.php
+++ input_update.php
@@ -60,6 +60,73 @@ $res	=  mysqli_query($real_sock,$sql) or die(mysqli_error($real_sock));
 
 
 
+$username = $t1;
+$maintext = $t1.'기업이 입사 제안이 수정했습니다. ';
+	class Slack {
+    
+		private $postData;
+		
+		public function __construct(){
+		}
+		
+		public function setPostData($postData){
+			$this->postData = $postData;
+		}
+		
+		public function sendSlack($postData) {
+			
+			$this->postData = $postData;
+			
+			if( isset($this->postData) == false || empty($this->postData) == true) {
+				// 데이터가 없으면 값을 보내지 않는다.
+				return false;
+			}
+			
+			try {
+								
+				//$ch = curl_init('https://hooks.slack.com/services/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');
+				$ch = curl_init('https://hooks.slack.com/services/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');    // 슬랙 테스트방
+				//$ch = curl_init('https://hooks.slack.com/services/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');    //정휘영
+				
+				
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST,  'POST');
+				curl_setopt($ch, CURLOPT_POSTFIELDS,     'payload='.json_encode($this->postData));
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+				
+				$result = curl_exec($ch);
+				curl_close($ch);
+				
+			} catch(Exception $e) {
+				$s = $e->getMessage() . ' (오류코드:' . $e->getCode() . ')';
+				//로깅처리
+			}
+			
+			return true;
+		}
+		
+	
+	
+	}
+	$slack = new Slack(); 
+	$postData = array( 'channel' => '#jira_site_slackapitest', 'username' => $username, 'text' =>  $maintext."
+	". date("Y-m-d H:i:s") ); 
+	
+	$slack->sendSlack($postData);
+
+
+
+	echo "<script>
+	alert('입사 제안해 주셔서 감사드립니다. 최대한 빨리 연락 드리겠습니다.');
+	
+	parent.location.replace('/Myjob/');
+	</script> ";
+
+
+
+
+
+
 
 }
 
