6883c5440f6919772d63a102f9fcf2f3045fc2ba
윤희동 <jini1237@naver.com>
2022-12-15 13:06:50 +0900

    Update dbcon.php
    업데이트

--- lib/dbcon.php
+++ lib/dbcon.php
@@ -15,7 +15,7 @@ $real_name="Myjob";
 $real_sock = mysqli_connect($real_hostname, $real_username, $real_password, $real_name);
 $real_db = mysqli_select_db($real_sock, $real_name) or die ("데이터베이스 서버에 연결할 수 없습니다.");
 mysqli_set_charset($real_sock, 'utf8'); 
-
+*/
 function replace_texts($text){
 	$text = str_replace('"' , '\"', $text);
 	$text = str_replace("'" , "\'", $text);	
@@ -1085,5 +1085,5 @@ function CliffNotes($title,$array){
 </div>
 <?php 
 }
-*/
+
 ?>
